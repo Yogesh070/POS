3fea92cc72a349f1f33aa7108b83a88354a8309b
sugat-udas <zitygt88@gmail.com>
2021-06-25 14:02:54 +0545

    storeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:pos/controller/settings_controller.dart';
+import 'package:pos/controller/storesController.dart';
 import 'package:pos/screens/settings.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => StoreController(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SettingController(),
+        ),
+      ],
+      child: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => SettingController(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: AppBarTheme(
-            backgroundColor: Colors.white,
-            iconTheme: IconThemeData(color: Colors.black),
-            foregroundColor: Colors.black,
-            titleTextStyle: TextStyle(color: Colors.red),
-          ),
-          primarySwatch: Colors.blue,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          foregroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.red),
         ),
-        home: Settings(),
+        primarySwatch: Colors.blue,
       ),
+      home: Settings(),
     );
   }
 }
