2925651ad61254cad59fb2682b6b33a023a269c7
Yogesh070 <ythapa430@gmail.com>
2021-06-29 17:21:57 +0545

    icon added to button

--- lib/components/secondary_button.dart
+++ lib/components/secondary_button.dart
@@ -3,23 +3,41 @@ import 'package:flutter/material.dart';
 class SecondaryButton extends StatelessWidget {
   final String? title;
   final VoidCallback? onPressed;
+  final Icon? icon;
+  final double? horizontalIconSpace;
+  final EdgeInsets? padding;
 
-  const SecondaryButton(
-      {Key? key, required this.title, required this.onPressed})
-      : super(key: key);
+  const SecondaryButton({
+    Key? key,
+    required this.title,
+    required this.onPressed,
+    this.horizontalIconSpace = 4,
+    this.icon,
+    this.padding,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: onPressed,
-      child: Text(title!),
+      child: Row(
+        children: [
+          icon ?? SizedBox.shrink(),
+          (icon != null)
+              ? SizedBox(
+                  width: horizontalIconSpace,
+                )
+              : SizedBox.shrink(),
+          Text(title!),
+        ],
+      ),
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(
           Color(0XFFDADADA),
         ),
         foregroundColor: MaterialStateProperty.all(Colors.black),
         padding: MaterialStateProperty.all(
-          EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          padding ?? EdgeInsets.symmetric(horizontal: 24, vertical: 8),
         ),
       ),
     );
