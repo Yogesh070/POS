5d9272cc9e64474f6190ac5c9199a1978855bdb8
Yogesh070 <ythapa430@gmail.com>
2021-09-19 14:38:35 +0545

    profile screen

---
+++ lib/screens/profile.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:pos/components/primary_button.dart';
+import 'package:pos/utilities/constant.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double mediaWidth = MediaQuery.of(context).size.width;
+    final childrenList1 = [
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.email),
+        title: Text('chelsi@gmail.com'),
+        horizontalTitleGap: 0,
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.phone_android),
+        horizontalTitleGap: 0,
+        title: Text('98234230423'),
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.location_on_outlined),
+        title: Text('Pokhara'),
+        horizontalTitleGap: 0,
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.message),
+        title: Text('Ok'),
+        horizontalTitleGap: 0,
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.receipt),
+        title: Text('Due Amt. (3500)'),
+        horizontalTitleGap: 0,
+      ),
+    ];
+    final childrenList2 = [
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.blur_circular_rounded),
+        title: Text('0.00'),
+        horizontalTitleGap: 0,
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.shop),
+        title: Text('3'),
+        horizontalTitleGap: 0,
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.calendar_today_outlined),
+        title: Text('21 Nov,2020'),
+        horizontalTitleGap: 0,
+      ),
+      ListTile(
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+        leading: Icon(Icons.star),
+        title: Text('Advance/Post Credit'),
+        horizontalTitleGap: 0,
+      ),
+    ];
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: (MediaQuery.of(context).size.width < 600)
+          ? AppBar(
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(4),
+                child: Divider(),
+              ),
+              elevation: 0,
+              title: Text(
+                'Profile',
+                style: kAppBarText,
+              ),
+              actions: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text(
+                      'ADD TO TICKET',
+                      style: TextStyle(
+                          color: kDefaultGreen,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16),
+                    ),
+                  ),
+                )
+              ],
+            )
+          : PreferredSize(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 16.0),
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Icon(Icons.arrow_back_ios)),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Customer Profile',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'ADD TO TICKET',
+                        style: TextStyle(
+                            color: kDefaultGreen,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              preferredSize: Size.fromHeight(60)),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: ListView(
+          padding: kScreenMargin,
+          children: [
+            CircleAvatar(
+              minRadius: 20,
+              child: Image.asset(
+                'assets/images/profile.png',
+                height: 60,
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Chelsi Ketan',
+                  style: (mediaWidth < 600)
+                      ? TextStyle(fontSize: 18, fontWeight: FontWeight.w500)
+                      : TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                ),
+                IconButton(
+                    splashRadius: 20,
+                    tooltip: 'Edit Profile',
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context)))
+                    }),
+              ],
+            ),
+            SizedBox(height: 16),
+            (mediaWidth < 600)
+                ? Column(
+                    children: [
+                      Column(children: childrenList1),
+                      Divider(),
+                      Column(children: childrenList2),
+                    ],
+                  )
+                : IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(child: Column(children: childrenList1)),
+                        Container(
+                          color: kBorderColor,
+                          width: 1,
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Expanded(
+                          child: Column(children: childrenList2),
+                        ),
+                      ],
+                    ),
+                  ),
+            SizedBox(
+              height: 16,
+            ),
+            Center(
+              child: Container(
+                width: 200,
+                child: PrimaryButton(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  title: 'Redeem Points',
+                  onPressed: () {},
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
