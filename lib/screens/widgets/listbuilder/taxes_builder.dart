3f5a6a9fa66af657d2a00069064652861fb7a4dd
Rajan Gurung <bardangrg0@gmail.com>
2021-09-27 11:30:47 +0545

    taxes builder for edit item screen

---
+++ lib/screens/widgets/listbuilder/taxes_builder.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pos/components/adds_on_tilte.dart';
+import 'package:pos/components/constrants.dart';
+import 'package:pos/controller/ticket.dart';
+
+class TaxBuilder extends StatelessWidget {
+  const TaxBuilder({
+    Key? key,
+    required TicketProvider controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final TicketProvider _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        physics: NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          final tax = _controller.taxes[index];
+          return AddsOnTile(
+            name: '${tax.taxName}',
+            isChecked: tax.isTaxChecked,
+            chechBoxCallback: (value) {
+              _controller.changeTaxeSwitchValue(index);
+            },
+            onTap: () {
+              _controller.changeTaxeSwitchValue(index);
+            },
+          );
+        },
+        separatorBuilder: (context, index) => divider,
+        itemCount: _controller.discounts.length);
+  }
+}
