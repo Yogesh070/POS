8c522e7c27c583dc5137418e51539c368aedf13a
Rajan Gurung <bardangrg0@gmail.com>
2021-09-27 11:25:48 +0545

    items ordered model

---
+++ lib/model/items_ordered_mode.dart
@@ -0,0 +1,11 @@
+class ItemsOrdered {
+  final String itemName;
+  final int itemQuantity;
+  final double itemRate;
+
+  ItemsOrdered({
+    required this.itemName,
+    required this.itemQuantity,
+    required this.itemRate,
+  });
+}
