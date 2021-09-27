c3f5b3d294d018ed862705878e7bddb3febc437e
Rajan Gurung <bardangrg0@gmail.com>
2021-09-27 11:31:00 +0545

    constrants

--- lib/components/constrants.dart
+++ lib/components/constrants.dart
@@ -6,3 +6,24 @@ var boxHeight = SizedBox(
 var boxWidth = SizedBox(
   width: 20,
 );
+
+const divider = Divider(thickness: 1);
+
+class LabelText extends StatelessWidget {
+  const LabelText({
+    Key? key,
+    required this.labelName,
+  }) : super(key: key);
+
+  final String labelName;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Text(
+        labelName,
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+}
