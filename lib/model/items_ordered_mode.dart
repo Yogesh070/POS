f9d3f707f0946da37fb1f175a4e8cdf11f8d5569
Rajan Gurung <bardangrg0@gmail.com>
2021-10-09 22:22:54 +0545

    ItemsOrdered Model

--- lib/model/items_ordered_mode.dart
+++ lib/model/items_ordered_mode.dart
@@ -2,10 +2,12 @@ class ItemsOrdered {
   final String itemName;
   final int itemQuantity;
   final double itemRate;
+  bool? isRefund;
 
   ItemsOrdered({
     required this.itemName,
     required this.itemQuantity,
     required this.itemRate,
+    this.isRefund = false,
   });
 }
